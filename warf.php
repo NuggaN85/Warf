404bc82a6a01a01600e895fa13783224348b349b
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-05-04 22:43:49 +0200

    Update warf.php
    Le code corrigé est maintenant plus facile à comprendre et à maintenir. Les fonctions séparent clairement les responsabilités et le code est plus robuste en utilisant `isset()` pour vérifier si les variables sont définies.

--- warf.php
+++ warf.php
@@ -4,51 +4,56 @@ define('TOKEN_EXPIRATION', 3600); // Durée de validité du jeton CSRF en second
 define('TOKEN_COOKIE_NAME', 'csrf_token'); // Nom du cookie CSRF
 define('TOKEN_SECRET_KEY', 'my_secret_key'); // Clé secrète pour signer le jeton CSRF
 
-// Vérification du jeton CSRF
 session_start();
-if ($_POST['token'] !== $_SESSION['token']) {
+
+function generate_csrf_token() {
+    $token = bin2hex(random_bytes(32));
+    $_SESSION['token'] = $token;
+    setcookie(TOKEN_COOKIE_NAME, $token, time() + TOKEN_EXPIRATION, '/', null, true, true);
+}
+
+function verify_csrf_token() {
+    if (isset($_COOKIE[TOKEN_COOKIE_NAME])) {
+        $cookie_token = $_COOKIE[TOKEN_COOKIE_NAME];
+        $signature = hash_hmac('sha256', $cookie_token, TOKEN_SECRET_KEY);
+        return hash_equals($signature, $_SESSION['token']);
+    }
+    return false;
+}
+
+function remove_expired_csrf_cookie() {
+    if (isset($_COOKIE[TOKEN_COOKIE_NAME])) {
+        $cookie_token = $_COOKIE[TOKEN_COOKIE_NAME];
+        $signature = hash_hmac('sha256', $cookie_token, TOKEN_SECRET_KEY);
+        if (!hash_equals($signature, $_SESSION['token'])) {
+            unset($_COOKIE[TOKEN_COOKIE_NAME]);
+            setcookie(TOKEN_COOKIE_NAME, null, -1, '/');
+        }
+    }
+}
+
+// Vérification du jeton CSRF
+if (isset($_POST['token']) && !verify_csrf_token()) {
     die('Jeton CSRF invalide.');
 }
 
 // Traitement du formulaire
-if ($_POST['submit']) {
+if (isset($_POST['submit'])) {
     // Faire quelque chose avec les données soumises
     // ...
     echo 'Formulaire soumis avec succès.';
 }
 
 // Génération d'un nouveau jeton CSRF
-$token = bin2hex(random_bytes(32));
-$_SESSION['token'] = $token;
-
-// Stockage du jeton CSRF dans un cookie sécurisé
-setcookie(TOKEN_COOKIE_NAME, $token, time() + TOKEN_EXPIRATION, '/', null, true, true);
-
-// Vérification du jeton CSRF à partir du cookie sécurisé
-if (!empty($_COOKIE[TOKEN_COOKIE_NAME])) {
-    $cookie_token = $_COOKIE[TOKEN_COOKIE_NAME];
-    $signature = hash_hmac('sha256', $cookie_token, TOKEN_SECRET_KEY);
-    if (hash_equals($signature, $_SESSION['token'])) {
-        // Le jeton CSRF est valide
-    } else {
-        // Le jeton CSRF est invalide
-    }
-}
+generate_csrf_token();
 
 // Suppression du cookie CSRF expiré
-if (!empty($_COOKIE[TOKEN_COOKIE_NAME])) {
-    $cookie_token = $_COOKIE[TOKEN_COOKIE_NAME];
-    $signature = hash_hmac('sha256', $cookie_token, TOKEN_SECRET_KEY);
-    if (!hash_equals($signature, $_SESSION['token'])) {
-        unset($_COOKIE[TOKEN_COOKIE_NAME]);
-        setcookie(TOKEN_COOKIE_NAME, null, -1, '/');
-    }
-}
+remove_expired_csrf_cookie();
 ?>
 
 <!-- Formulaire HTML avec jeton CSRF -->
 <form method="post">
-    <input type="hidden" name="token" value="<?php echo $token; ?>">
+    <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
     <input type="text" name="input1">
     <input type="text" name="input2">
     <input type="submit" name="submit" value="Soumettre">
